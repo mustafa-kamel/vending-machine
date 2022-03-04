19740ae88c273918754fa00e7db2dbbbc0d33d1f
Mustafa Kamel <mostafak252@gmail.com>
2022-03-04 23:04:37 +0200

    Fix app not found error on docker hypercorn server

--- startup.sh
+++ startup.sh
@@ -3,4 +3,4 @@
 python /opt/vending-machine/manage.py collectstatic --no-input
 python /opt/vending-machine/manage.py migrate
 
-hypercorn -b 0.0.0.0:${PORT} asgi:application
+hypercorn -b 0.0.0.0:${PORT} vending.asgi:application
