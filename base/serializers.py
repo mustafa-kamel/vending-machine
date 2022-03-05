80eefcf6d235ff293cef4d3a6364410b81a1435f
Mustafa Kamel <mostafak252@gmail.com>
2022-03-05 07:35:08 +0200

    Fix set user password on creation

--- base/serializers.py
+++ base/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import make_password
 from dynamic_rest.serializers import (DynamicModelSerializer,
                                       DynamicRelationField)
 
@@ -8,10 +9,13 @@ class UserSerializer(DynamicModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'deposit', 'role',
-                  'first_name', 'last_name', 'email']
+                  'first_name', 'last_name', 'email', 'password']
         extra_kwargs = {
             'password': {'write_only': True}
         }
+
+    def validate_password(self, value):
+        return make_password(value)
 
 
 class ProductSerializer(DynamicModelSerializer):
